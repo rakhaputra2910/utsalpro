c6e3b3c886d531d3926e450e933c2e4859ec08eb
rakhaputra2910 <115854656+rakhaputra2910@users.noreply.github.com>
2022-11-19 09:48:54 +0700

    Add files via upload

---
+++ Varaibel.java
@@ -0,0 +1,11 @@
+public class Varaibel {
+	public static void main(String[] args) {
+		String NAMA = "Rakha Digdaya Putra Wahana";
+		int TAHUN	= 2003;
+		System.out.println("Assalamu'alaikum");
+		System.out.println("Nama saya : " + NAMA);
+		System.out.println("Saya lahir di tahun : " + TAHUN);
+	}
+}
+
+
